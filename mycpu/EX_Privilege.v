3bbb74d54a9ef316908a4674a870f2e5df4cd09d
JYJSXX <95729892+JYJSXX@users.noreply.github.com>
2023-07-16 16:25:06 +0800

    privilege

---
+++ mycpu/EX_Privilege.v
@@ -0,0 +1,13 @@
+module EX_Privilege(
+    input clk,
+    input rstn,
+
+    input en,
+    input csr,
+    input [31:0] rd_data,
+    input [31:0] rj_data,
+    input [13:0] csr_num,
+    
+);
+
+endmodule
