13f224ae4c5756d4d36e92216fb530a893c2ea31
JYJSXX <95729892+JYJSXX@users.noreply.github.com>
2023-07-19 18:40:46 +0800

    Modified TLB

--- TLB/TLB.v
+++ TLB/TLB.v
@@ -3,8 +3,8 @@ module TLB(
     input [9:0]                 CSR_ASID,
     input [18:0]                CSR_VPPN,
 
-    input [`TLB_VPPN_LEN - 1:0] VA_I,
-    input [`TLB_VPPN_LEN - 1:0] VA_D,
+    input [`TLB_VPPN_LEN:0]     VA_I,
+    input [`TLB_VPPN_LEN:0]     VA_D,
     input                       PG,
     input [2:0]                 DMW0_VSEG,
     input [2:0]                 DMW1_VSEG,
@@ -44,6 +44,16 @@ generate
         assign rd_TLB_G[i]      =   tlb_cpr[i][`TLB_G];
         assign rd_TLB_PS[i]     =   tlb_cpr[i][`TLB_PS];
         assign rd_TLB_VPPN[i]   =   tlb_cpr[i][`TLB_VPPN];
+        assign rd_TLB_V_1[i]    =   tlb_trans_1[i][`TLB_V];
+        assign rd_TLB_D_1[i]    =   tlb_trans_1[i][`TLB_D];
+        assign rd_TLB_MAT_1[i]  =   tlb_trans_1[i][`TLB_MAT];
+        assign rd_TLB_PLV_1[i]  =   tlb_trans_1[i][`TLB_PLV];
+        assign rd_TLB_PPN_1[i]  =   tlb_trans_1[i][`TLB_PPN];
+        assign rd_TLB_V_2[i]    =   tlb_trans_2[i][`TLB_V];
+        assign rd_TLB_D_2[i]    =   tlb_trans_2[i][`TLB_D];
+        assign rd_TLB_MAT_2[i]  =   tlb_trans_2[i][`TLB_MAT];
+        assign rd_TLB_PLV_2[i]  =   tlb_trans_2[i][`TLB_PLV];
+        assign rd_TLB_PPN_2[i]  =   tlb_trans_2[i][`TLB_PPN];
     end
 endgenerate
 
@@ -62,8 +72,18 @@ wire [0:0] TLB_D_HIT [`TLB_NUM - 1:0];
 
 generate
     for(i = 0; i < `TLB_NUM; i = i + 1)begin
-        assign TLB_I_HIT[i] = rd_TLB_E[i] & (rd_TLB_G[i] | (rd_TLB_ASID[i] == CSR_ASID)) & (rd_TLB_V[i] == 1) && (rd_TLB_VPPN[i] == CSR_VPPN[`TLB_VPPN_LEN - 1:0]);
-        assign TLB_D_HIT[i] = rd_TLB_E[i] & (rd_TLB_G[i] | (rd_TLB_ASID[i] == CSR_ASID)) & (rd_TLB_V[i] == 1) && (rd_TLB_VPPN[i] == CSR_VPPN[`TLB_VPPN_LEN - 1:0]);
+        assign TLB_I_HIT[i] = rd_TLB_E[i] & (rd_TLB_G[i] | (rd_TLB_ASID[i] == CSR_ASID)) & (rd_TLB_VPPN[i] == VA_I[`TLB_VPPN_LEN:1]);
+        assign TLB_D_HIT[i] = rd_TLB_E[i] & (rd_TLB_G[i] | (rd_TLB_ASID[i] == CSR_ASID)) & (rd_TLB_VPPN[i] == VA_D[`TLB_VPPN_LEN:1]);
+    end
+endgenerate
+
+// reg:HIT UNHIT_EXCEPRION
+
+
+
+generate
+    for(i = 0; i < `TLB_NUM; i = i + 1)begin
+
     end
 endgenerate
 
