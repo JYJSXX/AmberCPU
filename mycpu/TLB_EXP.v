6ac9c4a743d8fb13942a80acf526ae6ac2189f94
fishsure <1055381810@qq.com>
2023-07-20 00:16:28 +0800

    tlb

---
+++ mycpu/TLB_EXP.v
@@ -0,0 +1,15 @@
+`include "../config.vh"
+`include "../exception.vh"
+
+module TLB_EXP(
+    input [31:0] vaddr0,
+    input en0,
+    input [1:0] plv0, //crmd_plv
+    input is_if_0,
+    input is_store_0,
+    input is_load_0,
+    
+
+    input [31:0] vaddr1,
+);
+endmodule
