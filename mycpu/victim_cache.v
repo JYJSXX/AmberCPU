52a7e975dbd785953beb687dfe4f41046093e736
Yiran <1378468839@qq.com>
2023-07-25 00:10:43 +0800

    victim_cache finish->module init

--- mycpu/victim_cache.v
+++ mycpu/victim_cache.v
@@ -33,23 +33,43 @@ module victim_cache#(
         end
     endgenerate
     //计数器，每次有写入时加1，不断循环
-    localparam COUNTER_WIDTH = $clog2(CAPACITY);
+    localparam COUNTER_WIDTH = funclog2(CAPACITY);
     reg [COUNTER_WIDTH-1:0] counter;
 
+    //读，类似于Dram，组合逻辑
+    wire [CAPACITY-1:0] hit;
+    generate
+        for(i = 0; i < CAPACITY; i = i + 1) begin: hit_gen
+            assign hit[i] = valid[i] && (tag[i][25:0] == r_tag[25:0]);
+        end
+    endgenerate
+    assign victim_hit = |hit;
+    assign data_out = data[hit];
+
+    //写，时序逻辑
+    integer j;
+    always @(posedge clk) begin
+        if(!rstn) begin
+            counter <= 0;
+            for(j = 0; j < CAPACITY; j = j + 1) begin
+                tag[j] <= 0;
+                data[j] <= 0;
+            end
+        end
+        else if(we) begin
+            counter <= counter + 1;
+            tag[counter] <= {1'b1, w_tag};
+            data[counter] <= data_in;
+        end
+    end
+
     function integer funclog2;
-
-   input integer value;
-
-   begin
-
-     value = value-1;
-
-     for (funclog2=0; value>0; funclog2=funclog2+1)
-
-       value = value>>1;
-
-   end
-
-Endfunction        
+        input integer value;
+        begin
+            value = value-1;
+            for (funclog2=0; value>0; funclog2=funclog2+1)
+                value = value>>1;
+        end
+    endfunction
 
 endmodule
