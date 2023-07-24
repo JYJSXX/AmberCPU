c688c8ef0091c7858482261e6ab716f08008da6e
YuriYZX <yzx_ustc@edu.cn>
2023-07-24 21:50:54 +0800

    icache_fix

--- mycpu/IF1_FIFO.v
+++ mycpu/IF1_FIFO.v
@@ -116,13 +116,14 @@ module IF1_FIFO(
     assign pc_from_PRIV = pc_after_priv;
 
 
-    always @(posedge clk or negedge rstn) begin
+    always @(posedge clk) begin
         if(!rstn)begin
             if1_fifo_pc_buf<=0;
             icache_rvalid_buf<=0;
-        end else if(flush)begin
+        end 
+        else if(flush)begin
             if1_fifo_pc_buf<=0;
-            icache_rvalid_buf<=0;
+            icache_rvalid_buf<=1;
         end
         else if(if1_allowin)begin
             if1_fifo_pc_buf<={if1_fifo_pc_buf[(WIDTH-1)*32-1:0],fetch_pc[31:0]};
