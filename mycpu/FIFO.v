7361626af3c4713d89f181a855394da1845168a8
YuriYZX <yzx_ustc@edu.cn>
2023-07-25 14:21:01 +0800

    fix FIFO

--- mycpu/FIFO.v
+++ mycpu/FIFO.v
@@ -7,8 +7,11 @@ module FIFO(
 
     //hand shake signal,stage DO NOT GEN SIGNAL!!!
     //stage read signal,write info to stage-stage regs
-    input fifo_readygo,
-    input fifo_allowin,
+    input           fifo_readygo,
+    output          fifo_allowin,
+    input           fifo_ready,
+    output          fifo_valid,
+    
 
     
     input [1 :0] priv_flag,
@@ -23,9 +26,6 @@ module FIFO(
     input [6 :0] if1_fifo_icache_exception,
     input [1:0]  if1_fifo_icache_excp_flag,
 
-    // input        if1_fifo_cacop_ready,
-    // input        if1_fifo_cacop_complete,
-
 
     output reg [31:0] fifo_inst0,
     output reg [31:0] fifo_inst1,
@@ -39,16 +39,13 @@ module FIFO(
     output reg [6 :0] fifo_exception, 
     output reg [1 :0] fifo_excp_flag,
     output reg [1 :0] fifo_priv_flag,
-    output reg [1 :0] fifo_branch_flag,
-
-    // output reg        fifo_cacop_ready,
-    // output reg        fifo_cacop_complete,
-
-    output fetch_buf_empty,
-    output fetch_buf_full
+    output reg [1 :0] fifo_branch_flag
+    
 );
     localparam BUF_DEPTH = 8;
     //if1_fifo_x->x_din->x_dout->fifo_x
+    wire fetch_buf_empty;
+    wire fetch_buf_full;
     wire write_en;
     wire pop_en;
     wire empty,full;
@@ -60,44 +57,30 @@ module FIFO(
     //[31:0]cookie  [38:32]exception [40:39]excp_flag [42:41]ibar_flag
     //[43:43]cacop_ready  [44:44]cacop_complete
 
-    assign empty                =   fetch_buf_empty;
-    assign full                 =   fetch_buf_full;
-    assign write_en             =   fifo_readygo;//do not use full/empty signal here,use it in stage-stage reg module to adjust allowin/readygo!!!
-    assign pop_en               =   fifo_allowin;
-    assign inst_din             =   {if1_fifo_inst1[31:0],if1_fifo_inst0[31:0]};
-    assign pcbdv_din            =   {
+    assign  empty               =   fetch_buf_empty;
+    assign  full                =   fetch_buf_full;
+    assign  write_en            =   fifo_readygo;                        
+    assign  pop_en              =   fifo_ready;
+    assign  inst_din            =   {if1_fifo_inst1[31:0],if1_fifo_inst0[31:0]};
+    assign  fifo_pcAdd          =   fifo_pc+4;
+    assign  pcbdv_din           =   {
                                         if1_fifo_pc_next[31:0],
                                         if1_fifo_pc[31:0],
                                         if1_fifo_icache_badv[31:0]
                                     };
-    assign stat_din             =   {
-                                        // if1_fifo_cacop_complete,
-                                        // if1_fifo_cacop_ready,
+    assign  stat_din            =   {
                                         branch_flag[1:0],
                                         priv_flag[1:0],
                                         if1_fifo_icache_excp_flag[1:0],
                                         if1_fifo_icache_exception[6:0],
                                         if1_fifo_icache_cookie_out[31:0]
                                     };
+    assign  fifo_valid          =   !fetch_buf_empty;
+    assign  fifo_allowin        =   !fetch_buf_full;
 
-    assign fifo_pcAdd=fifo_pc+4;
 
     always @(*) begin
-        if (empty&&write_en&&pop_en) begin//special case bypass
-            fifo_inst0=if1_fifo_inst0;
-            fifo_inst1=if1_fifo_inst1;
-            fifo_pc=if1_fifo_pc;
-            fifo_pc_next=if1_fifo_pc_next;
-
-            fifo_badv=if1_fifo_icache_badv;
-            fifo_cookie_out=if1_fifo_icache_cookie_out;
-            fifo_exception=if1_fifo_icache_exception;
-            fifo_excp_flag=if1_fifo_icache_excp_flag;
-            fifo_priv_flag=priv_flag;
-            fifo_branch_flag=branch_flag;
-            // fifo_cacop_ready=if1_fifo_cacop_ready;
-            // fifo_cacop_complete=if1_fifo_cacop_complete;
-        end else if(!empty)begin
+        if (fifo_valid)begin
             fifo_inst0=inst_dout[31:0];
             fifo_inst1=inst_dout[63:32];
             fifo_pc=pcbdv_dout[63:32];
@@ -109,8 +92,6 @@ module FIFO(
             fifo_excp_flag=stat_dout[40:39];
             fifo_priv_flag=stat_dout[42:41];
             fifo_branch_flag=stat_dout[44:43];
-            // fifo_cacop_ready=stat_dout[43:43];
-            // fifo_cacop_complete=stat_dout[44:44];
         end else begin
             fifo_inst0=`INST_NOP;
             fifo_inst1=`INST_NOP;
@@ -123,8 +104,6 @@ module FIFO(
             fifo_excp_flag=2'b00;
             fifo_priv_flag=2'b00;
             fifo_branch_flag=2'b00;
-            // fifo_cacop_ready=0;
-            // fifo_cacop_complete=0;
         end
     end 
 
@@ -170,11 +149,5 @@ module FIFO(
         .full       ( stat_full       ),
         .empty      ( stat_empty      )
     );
-
-
-
-
-
-    
 
 endmodule
