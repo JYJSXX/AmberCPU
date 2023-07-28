c2d8df7b91119eb528ee1ca436bb634eddc8eae9
Yiran <1378468839@qq.com>
2023-07-28 13:04:03 +0800

    temp_fifo_depth_enlarge

--- mycpu/FIFO.v
+++ mycpu/FIFO.v
@@ -43,8 +43,8 @@ module FIFO(
     output reg [1 :0] fifo_branch_flag
     
 );
-    localparam      BUF_DEPTH = 16,
-                    LOG_BUF_DEPTH = 4;
+    localparam      BUF_DEPTH = 64,
+                    LOG_BUF_DEPTH = 6;
     /*
             fifo_space=1:inst=3
             fifo_space=2:inst=57
