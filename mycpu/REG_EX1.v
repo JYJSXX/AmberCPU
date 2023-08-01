2c9e865e5e4865d17f8a0b3faa565b566f540747
Yiran <1378468839@qq.com>
2023-08-01 16:56:12 +0800

    MEMBUF Simplified

--- mycpu/REG_EX1.v
+++ mycpu/REG_EX1.v
@@ -57,14 +57,6 @@ module  REG_EX1(
     input [31:0] forward_data_k0,
     input [31:0] forward_data_j1,
     input [31:0] forward_data_k1,
-    input        tlb_forward_flag_j0,
-    input        tlb_forward_flag_k0,
-    input        tlb_forward_flag_j1,
-    input        tlb_forward_flag_k1,
-    input [31:0] tlb_forward_data_j0,
-    input [31:0] tlb_forward_data_k0,
-    input [31:0] tlb_forward_data_j1,
-    input [31:0] tlb_forward_data_k1,
     input         dcache_rready,
 
     output  reg [31:0] reg_ex_pc0,
@@ -245,10 +237,6 @@ reg forward_flag_j0_ps = 0;
 reg forward_flag_j1_ps = 0;
 reg forward_flag_k0_ps = 0;
 reg forward_flag_k1_ps = 0;
-reg tlb_forward_flag_j0_ps = 0;
-reg tlb_forward_flag_j1_ps = 0;
-reg tlb_forward_flag_k0_ps = 0;
-reg tlb_forward_flag_k1_ps = 0;
 always@(posedge clk) begin
     if(~aresetn || !stall_D) 
         forward_flag_j0_ps<=0;
@@ -285,37 +273,37 @@ always@(posedge clk) begin
 end
 always@(posedge clk) begin
     if(~aresetn || ex_allowin) 
-        tlb_forward_flag_j0_ps<=0;
-    else if(tlb_forward_flag_j0)
-        tlb_forward_flag_j0_ps <= 1;
+        forward_flag_j0_ps<=0;
+    else if(forward_flag_j0)
+        forward_flag_j0_ps <= 1;
     else
-        tlb_forward_flag_j0_ps <= tlb_forward_flag_j0_ps;
+        forward_flag_j0_ps <= forward_flag_j0_ps;
 end
 always@(posedge clk) begin
     if(~aresetn || ex_allowin) 
-        tlb_forward_flag_j1_ps<=0;
-    else if(tlb_forward_flag_j1)
-        tlb_forward_flag_j1_ps <= 1;
+        forward_flag_j1_ps<=0;
+    else if(forward_flag_j1)
+        forward_flag_j1_ps <= 1;
     else
-        tlb_forward_flag_j1_ps <= tlb_forward_flag_j1_ps;
-end
-
-always@(posedge clk) begin
-    if(~aresetn || ex_allowin) 
-        tlb_forward_flag_k0_ps<=0;
-    else if(tlb_forward_flag_k0)
-        tlb_forward_flag_k0_ps <= 1;
-    else
-        tlb_forward_flag_k0_ps <= tlb_forward_flag_k0_ps;
+        forward_flag_j1_ps <= forward_flag_j1_ps;
 end
 
 always@(posedge clk) begin
     if(~aresetn || ex_allowin) 
-        tlb_forward_flag_k1_ps<=0;
-    else if(tlb_forward_flag_k1)
-        tlb_forward_flag_k1_ps <= 1;
+        forward_flag_k0_ps<=0;
+    else if(forward_flag_k0)
+        forward_flag_k0_ps <= 1;
     else
-        tlb_forward_flag_k1_ps <= tlb_forward_flag_k1_ps;
+        forward_flag_k0_ps <= forward_flag_k0_ps;
+end
+
+always@(posedge clk) begin
+    if(~aresetn || ex_allowin) 
+        forward_flag_k1_ps<=0;
+    else if(forward_flag_k1)
+        forward_flag_k1_ps <= 1;
+    else
+        forward_flag_k1_ps <= forward_flag_k1_ps;
 end
 // reg priv_flag;
 always@(posedge clk)begin
@@ -452,10 +440,10 @@ always@(posedge clk)begin
         reg_ex_uop1 <= reg_ex_uop1;
         reg_ex_imm0 <= reg_ex_imm0;
         reg_ex_imm1 <= reg_ex_imm1;
-        reg_ex_rj0_data <=( tlb_forward_flag_j0) ? tlb_forward_data_j0 : reg_ex_rj0_data;
-        reg_ex_rj1_data <=(tlb_forward_flag_j1) ? tlb_forward_data_j1 : reg_ex_rj1_data;
-        reg_ex_rk0_data <= (tlb_forward_flag_k0) ? tlb_forward_data_k0 : reg_ex_rk0_data;
-        reg_ex_rk1_data <= (tlb_forward_flag_k1) ? tlb_forward_data_k1 : reg_ex_rk1_data;
+        reg_ex_rj0_data <=( forward_flag_j0) ? forward_data_j0 : reg_ex_rj0_data;
+        reg_ex_rj1_data <=(forward_flag_j1) ? forward_data_j1 : reg_ex_rj1_data;
+        reg_ex_rk0_data <= (forward_flag_k0) ? forward_data_k0 : reg_ex_rk0_data;
+        reg_ex_rk1_data <= (forward_flag_k1) ? forward_data_k1 : reg_ex_rk1_data;
         reg_ex_rj0 <= reg_ex_rj0;
         reg_ex_rj1 <= reg_ex_rj1;
         reg_ex_rk0 <= reg_ex_rk0;
