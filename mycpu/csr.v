4a5811061a2b740098a5fe424a049f329ba912bb
fishsure <yu12345@mail.ustc.edu.cn>
2023-07-31 20:02:00 -0700

    era

--- mycpu/csr.v
+++ mycpu/csr.v
@@ -428,7 +428,7 @@ always @(posedge clk)
         if(~aresetn) begin
             ecfg_lie <= 0;
         end else if(wen&&addr==`CSR_ECFG) begin
-           if(wen) ecfg_lie[`ECFG_LIE]<=wdata[`ECFG_LIE] ;
+           if(wen) ecfg_lie[`ECFG_LIE]<=wdata[`ECFG_LIE] & 13'b1_1011_1111_1111;
 
         end
     
@@ -907,7 +907,7 @@ assign rdata[31:0] = {32{addr_in==`CSR_CRMD}} & csr_crmd |
     wen&&addr==`CSR_CRMD ?  wdata  : pos_signal_excp ? {csr_crmd[31:3], 3'b000} :
                      ertn ? {csr_crmd[31:3],csr_prmd[2:0]}: csr_crmd,
     wen&&addr==`CSR_PRMD ? wdata  : pos_signal_excp ? {csr_prmd[31:3],csr_crmd[2:0]}:csr_prmd,
-    wen&&addr==`CSR_ECFG ? wdata:csr_ecfg ,
+    wen&&addr==`CSR_ECFG ? wdata & 32'b1_1011_1111_1111 :csr_ecfg & 32'b1_1011_1111_1111 ,
     wen&&addr==`CSR_ESTAT ? (wdata & 32'b11 | csr_estat & ~32'b11) : pos_signal_excp ? {csr_estat[31:23], expcode_in[6:0], csr_estat[15:0]} :csr_estat,
     wen&&addr==`CSR_ERA ? wdata : pos_signal_excp ? era_in : csr_era,
     wen&&addr==`CSR_BADV ? wdata :csr_badv,
