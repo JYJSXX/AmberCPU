4788590754fbdfab30f9b728aaa04d35d603556d
Yiran <1378468839@qq.com>
2023-08-04 09:07:53 +0800

    obj

---
+++ sim/obj/bitcount/test.s
@@ -0,0 +1,95 @@
+
+obj/bitcount/main.elf：     文件格式 elf32-loongarch
+obj/bitcount/main.elf
+
+
+Disassembly of section .text:
+
+1c000000 <_ftext>:
+_ftext():
+1c000000:	157f5f19 	lu12i.w	$r25,-263432(0xbfaf8)
+1c000004:	03bff339 	ori	$r25,$r25,0xffc
+1c000008:	29800320 	st.w	$r0,$r25,0
+1c00000c:	157f5ff9 	lu12i.w	$r25,-263425(0xbfaff)
+1c000010:	03bcc339 	ori	$r25,$r25,0xf30
+1c000014:	29800320 	st.w	$r0,$r25,0
+1c000018:	157f5f19 	lu12i.w	$r25,-263432(0xbfaf8)
+1c00001c:	03bff339 	ori	$r25,$r25,0xffc
+1c000020:	29800320 	st.w	$r0,$r25,0
+1c000024:	157f5f19 	lu12i.w	$r25,-263432(0xbfaf8)
+1c000028:	03bff339 	ori	$r25,$r25,0xffc
+1c00002c:	29800320 	st.w	$r0,$r25,0
+1c000030:	157f5ff9 	lu12i.w	$r25,-263425(0xbfaff)
+1c000034:	03bd0339 	ori	$r25,$r25,0xf40
+1c000038:	29800320 	st.w	$r0,$r25,0
+1c00003c:	157f5f19 	lu12i.w	$r25,-263432(0xbfaf8)
+1c000040:	03bff339 	ori	$r25,$r25,0xffc
+1c000044:	29800320 	st.w	$r0,$r25,0
+1c000048:	04060020 	csrwr	$r0,0x180
+1c00004c:	04060420 	csrwr	$r0,0x181
+1c000050:	0380640c 	ori	$r12,$r0,0x19
+1c000054:	0406002c 	csrwr	$r12,0x180
+1c000058:	1540000c 	lu12i.w	$r12,-393216(0xa0000)
+1c00005c:	0380258c 	ori	$r12,$r12,0x9
+1c000060:	0406042c 	csrwr	$r12,0x181
+1c000064:	0380400c 	ori	$r12,$r0,0x10
+1c000068:	0380600d 	ori	$r13,$r0,0x18
+1c00006c:	040001ac 	csrxchg	$r12,$r13,0x0
+1c000070:	1438000c 	lu12i.w	$r12,114688(0x1c000)
+1c000074:	038e018c 	ori	$r12,$r12,0x380
+1c000078:	0400302c 	csrwr	$r12,0xc
+1c00007c:	1c000003 	pcaddu12i	$r3,0
+1c000080:	288ca063 	ld.w	$r3,$r3,808(0x328)
+1c000084:	50030800 	b	776(0x308) # 1c00038c <go_finish>
+	...
+
+1c000100 <test_finish>:
+test_finish():
+1c000100:	0280058c 	addi.w	$r12,$r12,1(0x1)
+1c000104:	157f5fee 	lu12i.w	$r14,-263425(0xbfaff)
+1c000108:	03bc41ce 	ori	$r14,$r14,0xf10
+1c00010c:	298001c0 	st.w	$r0,$r14,0
+1c000110:	157f5fec 	lu12i.w	$r12,-263425(0xbfaff)
+1c000114:	0381818c 	ori	$r12,$r12,0x60
+1c000118:	2880018c 	ld.w	$r12,$r12,0
+1c00011c:	0342018c 	andi	$r12,$r12,0x80
+1c000120:	5800180c 	beq	$r0,$r12,24(0x18) # 1c000138 <test_finish+0x38>
+1c000124:	03400000 	andi	$r0,$r0,0x0
+1c000128:	157f5f0c 	lu12i.w	$r12,-263432(0xbfaf8)
+1c00012c:	2880018d 	ld.w	$r13,$r12,0
+1c000130:	50001400 	b	20(0x14) # 1c000144 <test_finish+0x44>
+1c000134:	03400000 	andi	$r0,$r0,0x0
+1c000138:	157f5f0c 	lu12i.w	$r12,-263432(0xbfaf8)
+1c00013c:	0380418c 	ori	$r12,$r12,0x10
+1c000140:	2880018d 	ld.w	$r13,$r12,0
+1c000144:	157f5fec 	lu12i.w	$r12,-263425(0xbfaff)
+1c000148:	0381418c 	ori	$r12,$r12,0x50
+1c00014c:	2980018d 	st.w	$r13,$r12,0
+1c000150:	53ffc3ff 	b	-64(0xfffffc0) # 1c000110 <test_finish+0x10>
+1c000154:	03400000 	andi	$r0,$r0,0x0
+1c000158:	1500000c 	lu12i.w	$r12,-524288(0x80000)
+1c00015c:	028005ad 	addi.w	$r13,$r13,1(0x1)
+1c000160:	0015018e 	move	$r14,$r12
+1c000164:	00104a2f 	add.w	$r15,$r17,$r18
+1c000168:	28800190 	ld.w	$r16,$r12,0
+	...
+1c000380:	0280058c 	addi.w	$r12,$r12,1(0x1)
+1c000384:	53ffffff 	b	-4(0xffffffc) # 1c000380 <test_finish+0x280>
+1c000388:	03400000 	andi	$r0,$r0,0x0
+
+1c00038c <go_finish>:
+run_test():
+1c00038c:	53fd77ff 	b	-652(0xffffd74) # 1c000100 <test_finish>
+
+Disassembly of section .data:
+
+1c000390 <__CTOR_LIST__>:
+	...
+
+1c000398 <__CTOR_END__>:
+	...
+
+1c0003a0 <__DTOR_END__>:
+__DTOR_END__():
+1c0003a0:	00000000 	0x00000000
+1c0003a4:	1c010370 	pcaddu12i	$r16,2075(0x81b)
