0917d987bbd091b6d05ffaff212a3564d164fe9e
JYJSXX <liuruibo21@mail.ustc.edu.cn>
2023-08-05 10:24:55 +0800

    divider pass

--- mycpu/divider.v
+++ mycpu/divider.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ps
-// `define OURDIV
+`define OURDIV
 `ifdef OURDIV
 module divider(
     input                           clk,
@@ -61,17 +61,18 @@ module divider(
         end
         else
         begin
-            divisor_reg <= {32'b0, divisor_abs} << (digit_dividend - digit_divisor + 1);
+            divisor_reg <= {32'b0, divisor_abs} << (digit_dividend_reg - digit_divisor_reg + 1);
             shift_count <= 0;
         end
     end
 
     localparam [2:0]
                     DIV_IDLE = 0,
+                    DIV_REG = 6,
                     DIV_INIT = 1,
                     DIV_CALC = 2,
                     DIV_DONE = 3,
-                    DIV_HOLD = 4,
+                    // DIV_HOLD = 4,
                     DIV_RESULT_PREPARE = 5;
 
     reg     [2:0]   div_state = DIV_IDLE, div_next_state = DIV_IDLE;
@@ -95,17 +96,18 @@ module divider(
             DIV_IDLE:
             begin
                 if(en)
-                    div_next_state = DIV_INIT;
+                    div_next_state = DIV_REG;
             end
+            DIV_REG: div_next_state = DIV_INIT;
             DIV_INIT:
             begin
                 if(digit_divisor_reg > digit_dividend_reg || (~|digit_divisor_reg))
-                    div_next_state = DIV_HOLD;
+                    div_next_state = DIV_DONE;
                 else 
                     div_next_state = DIV_CALC;
             end
-            DIV_HOLD:
-            div_next_state = DIV_DONE;
+            // DIV_HOLD:
+            // div_next_state = DIV_DONE;
             DIV_CALC:
             begin
                 if(shift_count == digit_dividend_reg - digit_divisor_reg + 1)
@@ -162,7 +164,7 @@ module divider(
                         dividend_reg <= {32'b0, dividend_abs};
                         dividend_sign <= dividend[31];
                         divisor_sign <= divisor[31];
-                        shift <= 1;
+                        shift <= 0;
                     end
 
                     else
@@ -176,6 +178,9 @@ module divider(
                         shift <= 0;
                     end
                 end
+                DIV_REG:begin
+                    shift <= 1;
+                end
                 DIV_INIT:
                 begin
                     digit_dividend_reg <= digit_dividend_reg;
@@ -184,33 +189,33 @@ module divider(
                     dividend_reg <= dividend_reg;
                     dividend_sign <= dividend_sign;
                     divisor_sign <= divisor_sign;
-                    // if(digit_divisor_reg > digit_dividend_reg || (~|digit_divisor_reg))
-                    // begin
-                    //     shift <= 0;
-                    //     quotient <= 0;
-                    //     remainder <= (sign_reg & dividend_sign) ? ~dividend_reg[31:0] + 1 : dividend_reg[31:0];
-                    // end
-                    // else
-                    if(~(digit_divisor_reg > digit_dividend_reg || (~|digit_divisor_reg)))
+                    if(digit_divisor_reg > digit_dividend_reg || (~|digit_divisor_reg))
+                    begin
+                        shift <= 0;
+                        quotient <= 0;
+                        remainder <= (sign_reg & dividend_sign) ? ~dividend_reg[31:0] + 1 : dividend_reg[31:0];
+                    end
+                    else
+                    // if(~(digit_divisor_reg > digit_dividend_reg || (~|digit_divisor_reg)))
                     begin
                         shift <= 1;
                         quotient <= 0;
                         remainder <= 0;
                     end
                 end
-                DIV_HOLD:
-                begin
-                    shift <= 0;
-                    quotient <= 0;
-                    remainder <= (sign_reg & dividend_sign) ? ~dividend_reg[31:0] + 1 : dividend_reg[31:0];
-                end
+                // DIV_HOLD:
+                // begin
+                //     shift <= 0;
+                //     quotient <= 0;
+                //     remainder <= (sign_reg & dividend_sign) ? ~dividend_reg[31:0] + 1 : dividend_reg[31:0];
+                // end
                 DIV_CALC:
                 begin
                     if(shift_count == digit_dividend_reg - digit_divisor_reg + 1)
                     begin
                         shift <= 0;
-                        quotient <= sign_reg & (dividend_sign ^ divisor_sign) ? ~quotient_nxt + 1 : quotient_nxt;
-                        remainder <= (sign_reg & dividend_sign) ? ~(minus[63] ? dividend_reg[31:0] : minus[31:0]) + 1 : (minus[63] ? dividend_reg[31:0] : minus[31:0]);
+                        // quotient <= sign_reg & (dividend_sign ^ divisor_sign) ? ~quotient_nxt + 1 : quotient_nxt;
+                        // remainder <= (sign_reg & dividend_sign) ? ~(minus[63] ? dividend_reg[31:0] : minus[31:0]) + 1 : (minus[63] ? dividend_reg[31:0] : minus[31:0]);
                     end
 
                     else
